ede9eea2e2cd1b945b10d46d874762d31e0b087a
Dylan Curvey <dcurves@Dylans-MacBook-Pro.local>
2017-04-30 20:42:05 -0400

    pair<string, vector<string>>

--- neko.cpp
+++ neko.cpp
@@ -5,129 +5,156 @@
 #include <utility> 
 #include <vector>
 #include <ctime>
+#include <unordered_map>
 
 using namespace std;
 
 void inc(string&, int);
 
-vector<pair<string, string>> hiragana{};
+vector<pair<string, vector<string>>> hiragana{};
+//unordered_map<string, vector<string>> hiragana;
 
 
 int main(int argc, char** argv)
 {
 
-	hiragana.push_back(make_pair("あ", "a"));
-	hiragana.push_back(make_pair("え", "e"));
-	hiragana.push_back(make_pair("い", "i"));
-	hiragana.push_back(make_pair("お", "o"));
-	hiragana.push_back(make_pair("う", "u"));
+	hiragana.push_back(make_pair("あ", vector<string> {"a"}));
+	hiragana.push_back(make_pair("え", vector<string> {"e"}));
+	hiragana.push_back(make_pair("い", vector<string> {"i"}));
+	hiragana.push_back(make_pair("お", vector<string> {"o"}));
+	hiragana.push_back(make_pair("う", vector<string> {"u"}));
 
-	hiragana.push_back(make_pair("こ", "ko"));
-	hiragana.push_back(make_pair("か", "ka"));
-	hiragana.push_back(make_pair("く", "ku"));
-	hiragana.push_back(make_pair("き", "ki"));
-	hiragana.push_back(make_pair("け", "ke"));
+	hiragana.push_back(make_pair("こ", vector<string> {"ko"}));
+	hiragana.push_back(make_pair("か", vector<string> {"ka"}));
+	hiragana.push_back(make_pair("く", vector<string> {"ku"}));
+	hiragana.push_back(make_pair("き", vector<string> {"ki"}));
+	hiragana.push_back(make_pair("け", vector<string> {"ke"}));
 
-	hiragana.push_back(make_pair("さ", "sa"));
-	hiragana.push_back(make_pair("す", "su"));
-	//hiragana.push_back(make_pair("し", "si"));
-	hiragana.push_back(make_pair("せ", "se"));
-	hiragana.push_back(make_pair("そ", "so"));
+	hiragana.push_back(make_pair("さ", vector<string> {"sa"}));
+	hiragana.push_back(make_pair("す", vector<string> {"su"}));
+	hiragana.push_back(make_pair("せ", vector<string> {"se"}));
+	hiragana.push_back(make_pair("そ", vector<string> {"so"}));
 
-	hiragana.push_back(make_pair("と", "to"));
-	hiragana.push_back(make_pair("た", "ta"));
-	//hiragana.push_back(make_pair("ち", "ti"));
-	//hiragana.push_back(make_pair("つ", "tu"));
-	hiragana.push_back(make_pair("て", "te"));
+	hiragana.push_back(make_pair("と", vector<string> {"to"}));
+	hiragana.push_back(make_pair("た", vector<string> {"ta"}));
+	hiragana.push_back(make_pair("て", vector<string> {"te"}));
 
-	hiragana.push_back(make_pair("な", "na"));
-	hiragana.push_back(make_pair("に", "ni"));
-	hiragana.push_back(make_pair("ぬ", "nu"));
-	hiragana.push_back(make_pair("ね", "ne"));
-	hiragana.push_back(make_pair("の", "no"));
+	hiragana.push_back(make_pair("な", vector<string> {"na"}));
+	hiragana.push_back(make_pair("に", vector<string> {"ni"}));
+	hiragana.push_back(make_pair("ぬ", vector<string> {"nu"}));
+	hiragana.push_back(make_pair("ね", vector<string> {"ne"}));
+	hiragana.push_back(make_pair("の", vector<string> {"no"}));
 
-	hiragana.push_back(make_pair("は", "ha"));
-	hiragana.push_back(make_pair("ひ", "hi"));
-	hiragana.push_back(make_pair("ふ", "hu"));
-	hiragana.push_back(make_pair("へ", "he"));
-	hiragana.push_back(make_pair("ほ", "ho"));
+	hiragana.push_back(make_pair("は", vector<string> {"ha"}));
+	hiragana.push_back(make_pair("ひ", vector<string> {"hi"}));
+	hiragana.push_back(make_pair("ふ", vector<string> {"hu"}));
+	hiragana.push_back(make_pair("へ", vector<string> {"he"}));
+	hiragana.push_back(make_pair("ほ", vector<string> {"ho"}));
 
-	hiragana.push_back(make_pair("ま", "ma"));
-	hiragana.push_back(make_pair("み", "mi"));
-	hiragana.push_back(make_pair("む", "mu"));
-	hiragana.push_back(make_pair("め", "me"));
-	hiragana.push_back(make_pair("も", "mo"));
+	hiragana.push_back(make_pair("ま", vector<string> {"ma"}));
+	hiragana.push_back(make_pair("み", vector<string> {"mi"}));
+	hiragana.push_back(make_pair("む", vector<string> {"mu"}));
+	hiragana.push_back(make_pair("め", vector<string> {"me"}));
+	hiragana.push_back(make_pair("も", vector<string> {"mo"}));
 
-	hiragana.push_back(make_pair("や", "ya"));
-	hiragana.push_back(make_pair("ゆ", "yu"));
-	hiragana.push_back(make_pair("よ", "yo"));
+	hiragana.push_back(make_pair("や", vector<string> {"ya"}));
+	hiragana.push_back(make_pair("ゆ", vector<string> {"yu"}));
+	hiragana.push_back(make_pair("よ", vector<string> {"yo"}));
 
-	hiragana.push_back(make_pair("ら", "ra"));
-	hiragana.push_back(make_pair("り", "ri"));
-	hiragana.push_back(make_pair("る", "ru"));
-	hiragana.push_back(make_pair("れ", "re"));
-	hiragana.push_back(make_pair("ろ", "ro"));
+	hiragana.push_back(make_pair("ら", vector<string> {"ra"}));
+	hiragana.push_back(make_pair("り", vector<string> {"ri"}));
+	hiragana.push_back(make_pair("る", vector<string> {"ru"}));
+	hiragana.push_back(make_pair("れ", vector<string> {"re"}));
+	hiragana.push_back(make_pair("ろ", vector<string> {"ro"}));
 
-	hiragana.push_back(make_pair("わ", "wa"));
-	hiragana.push_back(make_pair("を", "wo"));
+	hiragana.push_back(make_pair("わ", vector<string> {"wa"}));
+	hiragana.push_back(make_pair("を", vector<string> {"wo"}));
 
-	hiragana.push_back(make_pair("ざ", "za"));
-	hiragana.push_back(make_pair("じ", "zi"));
-	hiragana.push_back(make_pair("ず", "zu"));
-	hiragana.push_back(make_pair("ぜ", "ze"));
-	hiragana.push_back(make_pair("ぞ", "zo"));
+	hiragana.push_back(make_pair("ざ", vector<string> {"za"}));
+	hiragana.push_back(make_pair("ず", vector<string> {"zu"}));
+	hiragana.push_back(make_pair("ぜ", vector<string> {"ze"}));
+	hiragana.push_back(make_pair("ぞ", vector<string> {"zo"}));
 
-	hiragana.push_back(make_pair("が", "ga"));
-	hiragana.push_back(make_pair("ぎ", "gi"));
-	hiragana.push_back(make_pair("ぐ", "gu"));
-	hiragana.push_back(make_pair("げ", "ge"));
-	hiragana.push_back(make_pair("ご", "go"));
+	hiragana.push_back(make_pair("が", vector<string> {"ga"}));
+	hiragana.push_back(make_pair("ぎ", vector<string> {"gi"}));
+	hiragana.push_back(make_pair("ぐ", vector<string> {"gu"}));
+	hiragana.push_back(make_pair("げ", vector<string> {"ge"}));
+	hiragana.push_back(make_pair("ご", vector<string> {"go"}));
 
-	hiragana.push_back(make_pair("だ", "da"));
-	hiragana.push_back(make_pair("ぢ", "di"));
-	hiragana.push_back(make_pair("づ", "du"));
-	hiragana.push_back(make_pair("で", "de"));
-	hiragana.push_back(make_pair("ど", "do"));
+	hiragana.push_back(make_pair("だ", vector<string> {"da"}));
+	hiragana.push_back(make_pair("ぢ", vector<string> {"di"}));
+	hiragana.push_back(make_pair("づ", vector<string> {"du"}));
+	hiragana.push_back(make_pair("で", vector<string> {"de"}));
+	hiragana.push_back(make_pair("ど", vector<string> {"do"}));
 
-	hiragana.push_back(make_pair("ば", "ba"));
-	hiragana.push_back(make_pair("び", "bi"));
-	hiragana.push_back(make_pair("ぶ", "bu"));
-	hiragana.push_back(make_pair("べ", "be"));
-	hiragana.push_back(make_pair("ぼ", "bo"));
+	hiragana.push_back(make_pair("ば", vector<string> {"ba"}));
+	hiragana.push_back(make_pair("び", vector<string> {"bi"}));
+	hiragana.push_back(make_pair("ぶ", vector<string> {"bu"}));
+	hiragana.push_back(make_pair("べ", vector<string> {"be"}));
+	hiragana.push_back(make_pair("ぼ", vector<string> {"bo"}));
 
-	hiragana.push_back(make_pair("ぱ", "pa"));
-	hiragana.push_back(make_pair("ぷ", "pu"));
-	hiragana.push_back(make_pair("ぴ", "pi"));
-	hiragana.push_back(make_pair("ぺ", "pe"));
-	hiragana.push_back(make_pair("ぽ", "po"));
+	hiragana.push_back(make_pair("ぱ", vector<string> {"pa"}));
+	hiragana.push_back(make_pair("ぷ", vector<string> {"pu"}));
+	hiragana.push_back(make_pair("ぴ", vector<string> {"pi"}));
+	hiragana.push_back(make_pair("ぺ", vector<string> {"pe"}));
+	hiragana.push_back(make_pair("ぽ", vector<string> {"po"}));
 
-	hiragana.push_back(make_pair("ん", "n"));
+	hiragana.push_back(make_pair("ん", vector<string> {"n"}));
 	
-	hiragana.push_back(make_pair("ち", "chi"));
-	hiragana.push_back(make_pair("し", "shi"));
-	hiragana.push_back(make_pair("つ", "tsu"));
+	hiragana.push_back(make_pair("じ", vector<string> {"zi", "ji"}));
+	hiragana.push_back(make_pair("ち", vector<string> {"chi", "ti"}));
+	hiragana.push_back(make_pair("し", vector<string> {"shi", "si"}));
+	hiragana.push_back(make_pair("つ", vector<string> {"tsu", "tu"}));
 
 
 	cout << "\n\nPractice Hiragana, type \"q\", \"quit\", or \"exit\" to quit, or \"help\" for a reference list. If you want to reveal the answer, type \"ans\"" << "\n" << "Ganbatte!\n\n";
 	string in;
 	while(in != "exit" && in != "quit" && in != "q")
 	{
+		bool is_correct = false;
 		srand(time(NULL));
 		int num = rand() % hiragana.size();
 		cout << "\n" << hiragana[num].first << "\n\n";
 		cin >> in;
-		while(in != hiragana[num].second)
+		while(!is_correct)
 		{
 			if(in == "ans")
 			{
-				cout << "\nThe answer is: " << hiragana[num].first << " = " << hiragana[num].second << "\n\n";
+				cout << "\nThe answer is: " << hiragana[num].first << " = ";
+				for(int x = 0; x < hiragana[num].second.size(); ++x)
+				{
+					cout << hiragana[num].second[x];
+
+					if(x != (hiragana[num].second.size() - 1))
+					{
+						cout << " or ";
+					}
+					else
+					{
+						cout << "\n\n";
+					}
+				}
 				break;
 			}
 			else if(in == "help")
 			{
 				for(int i = 0; i < hiragana.size(); ++i)
 				{
-					cout << hiragana[i].first << " = " << hiragana[i].second << "		";
+					cout << hiragana[i].first << " = ";
+
+					for(int x = 0; x < hiragana[i].second.size(); ++x)
+					{
+						cout << hiragana[i].second[x];
+
+						if(x != (hiragana[i].second.size() - 1))
+						{
+							cout << ", ";
+						}
+						else
+						{
+							cout << "		";
+						}
+					}
 					if(i % 5 == 4)
 					{
 						cout << "\n";
@@ -143,7 +170,18 @@ int main(int argc, char** argv)
 			}
 			else
 			{
-				inc(in, num);
+				for(int x = 0; x < hiragana[num].second.size(); ++x)
+				{
+					if(hiragana[num].second[x] == in)
+					{
+						is_correct = true;
+						break;
+					}
+				}
+				if(!is_correct)
+				{
+					inc(in, num);
+				}
 			}
 		}
 		if(in == "ans")
